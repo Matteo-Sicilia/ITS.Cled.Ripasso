6cea377d31e010780fb7633a5cf86865789b1ea9
Matteo Sicilia <matteo.sicilia@outlook.it>
2024-10-31 12:05:40 +0100

    add razor pages for product creation

--- ITS.Cled.Ripasso/Model/Product.cs
+++ ITS.Cled.Ripasso/Model/Product.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ITS.Cled.Ripasso.Model;
 
 using Dapper.Contrib.Extensions;
@@ -7,7 +9,16 @@ public class Product
 {
     [Key]
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public string Name { get; set; } = default!;
+
+    [Required]
+    [StringLength(10)]
     public string Code { get; set; } = default!;
-    public decimal Price { get; set; } = default!;
+
+    [Required]
+    [Range(0, double.MaxValue)]
+    public decimal Price { get; set; }
 }
